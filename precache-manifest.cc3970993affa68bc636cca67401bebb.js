d720191d0291944a0c8485c79e1d8438864b2fcb
nataliiaGrineva <grineva.nataliia@gmail.com>
2022-03-23 17:05:53 +0200

    Updates

---
+++ precache-manifest.cc3970993affa68bc636cca67401bebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e7806b23841762e014ece4d9d7c94d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c86d130eea8ae2e2944",
+    "url": "./static/css/main.96f8354a.chunk.css"
+  },
+  {
+    "revision": "87e6030f0ec97dfabf68",
+    "url": "./static/js/2.e5269da5.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.e5269da5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c86d130eea8ae2e2944",
+    "url": "./static/js/main.3410f892.chunk.js"
+  },
+  {
+    "revision": "35878c0637cea8221062",
+    "url": "./static/js/runtime-main.ba67aef9.js"
+  }
+]);
